cab99c2b0d08006b0780e64e86e9fbad00faaec2
mufty <gubriansky@sernato.sk>
2021-07-14 18:16:00 +0200

    git ignore and manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'mufty'
+description 'MRP Employment'
+version '0.0.1'
+
+dependencies {
+    "mrp_core"
+}
+
+shared_scripts {
+    '@mrp_core/shared/debug.js',
+}
+
+client_scripts {
+    'client/*.js',
+}
+
+server_scripts {
+    'server/*.js',
+}
