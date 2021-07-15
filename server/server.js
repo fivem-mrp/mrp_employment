5e1bbf042b7dc5ea4da0f4729d7c46ca0aadc794
mufty <gubriansky@sernato.sk>
2021-07-15 23:04:03 +0200

    shared functions will be useful down the road

--- server/server.js
+++ server/server.js
@@ -11,88 +11,89 @@ while (MRP_SERVER == null) {
     console.log('Waiting for shared object....');
 }
 
-function findEmployement(data, business, role) {
-    let employment;
-    for (let emp of data.employment) {
-        if (emp.business == business && emp.role == role)
-            employment = emp;
-    }
-    return employment;
-}
-
-function addEmployment(source, stateId, businessId, jobName) {
-    MRP_SERVER.read('character', {
-        stateId: stateId
-    }, (char) => {
-        if (!char) {
-            emitNet('chat:addMessage', source, {
-                template: '<div class="chat-message nonemergency">{0}</div>',
-                args: [
-                    locale.errorWLChar
-                ]
-            });
-            console.log('Unable to find a character to whitelist');
-            return;
+MRP_SERVER.employment = {
+    findEmployement: (data, business, role) => {
+        let employment;
+        for (let emp of data.employment) {
+            if (emp.business == business && emp.role == role)
+                employment = emp;
         }
-
-        MRP_SERVER.read('employment', {
-            char: char._id
-        }, (data) => {
-            let needUpdate = false;
-            if (!data) {
-                //unemployed
-                data = {
-                    char: char._id,
-                    employment: [{
-                        business: businessId,
-                        role: jobName
-                    }]
-                };
-                needUpdate = true;
-            } else {
-                let job = findEmployement(data, businessId, jobName);
-                if (job) {
-                    console.log(`Player with state ID [${stateId}] already has a job [${jobName}]`);
-                    emitNet('chat:addMessage', source, {
-                        template: '<div class="chat-message nonemergency">{0}</div>',
-                        args: [
-                            locale.errorAlreadyHasJob.replace('${stateId}', stateId).replace('${jobName}', jobName)
-                        ]
-                    });
-                    return;
-                }
-
-                needUpdate = true;
-                data.employment.push({
-                    business: businessId,
-                    role: jobName
+        return employment;
+    },
+    addEmployment: (source, stateId, businessId, jobName) => {
+        MRP_SERVER.read('character', {
+            stateId: stateId
+        }, (char) => {
+            if (!char) {
+                emitNet('chat:addMessage', source, {
+                    template: '<div class="chat-message nonemergency">{0}</div>',
+                    args: [
+                        locale.errorWLChar
+                    ]
                 });
+                console.log('Unable to find a character to whitelist');
+                return;
             }
 
-            if (needUpdate) {
-                let query = {};
-
-                if (data._id)
-                    query._id = data._id;
-
-                MRP_SERVER.update('employment', data, query, null, (result) => {
-                    if (result.modifiedCount > 0) {
-                        console.log(`Updated employment for state ID [${stateId}] with job name [${jobName}]`);
-                    } else {
-                        console.log(`Added employment for state ID [${stateId}] with job name [${jobName}]`);
+            MRP_SERVER.read('employment', {
+                char: char._id
+            }, (data) => {
+                let needUpdate = false;
+                if (!data) {
+                    //unemployed
+                    data = {
+                        char: char._id,
+                        employment: [{
+                            business: businessId,
+                            role: jobName
+                        }]
+                    };
+                    needUpdate = true;
+                } else {
+                    let job = MRP_SERVER.employment.findEmployement(data, businessId, jobName);
+                    if (job) {
+                        console.log(`Player with state ID [${stateId}] already has a job [${jobName}]`);
+                        emitNet('chat:addMessage', source, {
+                            template: '<div class="chat-message nonemergency">{0}</div>',
+                            args: [
+                                locale.errorAlreadyHasJob.replace('${stateId}', stateId).replace('${jobName}', jobName)
+                            ]
+                        });
+                        return;
                     }
 
-                    emitNet('chat:addMessage', source, {
-                        template: '<div class="chat-message nonemergency">{0}</div>',
-                        args: [
-                            locale.wlSuccess.replace('${stateId}', stateId).replace('${jobName}', jobName)
-                        ]
+                    needUpdate = true;
+                    data.employment.push({
+                        business: businessId,
+                        role: jobName
                     });
-                });
-            }
+                }
+
+                if (needUpdate) {
+                    let query = {};
+
+                    if (data._id)
+                        query._id = data._id;
+
+                    MRP_SERVER.update('employment', data, query, null, (result) => {
+                        if (result.modifiedCount > 0) {
+                            console.log(`Updated employment for state ID [${stateId}] with job name [${jobName}]`);
+                        } else {
+                            console.log(`Added employment for state ID [${stateId}] with job name [${jobName}]`);
+                        }
+
+                        emitNet('chat:addMessage', source, {
+                            template: '<div class="chat-message nonemergency">{0}</div>',
+                            args: [
+                                locale.wlSuccess.replace('${stateId}', stateId).replace('${jobName}', jobName)
+                            ]
+                        });
+                    });
+                }
+            });
         });
-    });
-}
+    }
+};
 
 RegisterCommand('wl', (source, args) => {
     //whitelist command
@@ -103,5 +104,5 @@ RegisterCommand('wl', (source, args) => {
     if (!jobName)
         return;
 
-    addEmployment(source, stateId, 'city', jobName); //not sure if we only allow city whitelist for now
+    MRP_SERVER.employment.addEmployment(source, stateId, 'city', jobName); //not sure if we only allow city whitelist for now
 }, true);
